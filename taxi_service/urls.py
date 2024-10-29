92d64e8d495881bca53ce1a83d7eb4a8e94ad95d
Yuliia Dudych <juliadudych@gmail.com>
2024-10-29 14:37:06 +0200

    Remove debug_toolbar import

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,8 +19,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from debug_toolbar.toolbar import debug_toolbar_urls
-
 
 urlpatterns = (
     [
